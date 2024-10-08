bd2c3ec60287aaea0ac53a0dc095f4b9ebf50174
edenis00 <elijahdenis540@gmail.com>
2024-10-08 12:24:58 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+"""
+ Importing Packages
+"""
+
+from flask import Flask
+from flask_migrate import Migrate
+from extension import db
+from routes.user_routes import user_routes
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/devDb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = SQLAlchemy(app)
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
+app.register_blueprint(user_routes, url_prefix='/users')
+
+if __name__ == "__main__":
+    app.run(debug=True)
